e03d747c1d479f463c159ae2530d40792cca1be0
Owen Mariani <mariani.owen@gmail.com>
2025-09-04 13:44:21 -0400

    created user_exists and get_repos endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,37 @@
 from fastapi import FastAPI
-import typing
-
+import requests
+import json
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/user/{username}/exists")
+async def user_exists(username:str):
+    exists = False
+    try:
+        headers = {
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28',
+        }
+        response = requests.get(url=f"https://api.github.com/users/{username}", headers=headers).json()
+        
+        if response.get("login"):
+            message = f"User {username} exists"
+            exists = True
+        else:
+            message = f"User {username} does not exist"
+    except Exception as e:
+        message = f"Something went wrong. Got this exception: {e}"
+    return {"message": message, "exists": exists}
+
 @app.get('/user/{username}/repos')
-def get_repos(username: str) -> typing.Dict:
-    return {"message": f"{username}'s repos"}
+def get_repos(username: str):
+    repositories = requests.get(url=f"https://api.github.com/users/{username}/repos").json()
+
+    return {"message": f"{username}'s repos", "repositories": repositories}
 
 # @app.route('')
 # def get_languages():
